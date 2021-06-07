ed031ce9fa1dfb3279f98b3d2639739f038ae380
DESKTOP-QUPUQ2M\Minja <mrcha993@gmail.com>
2021-06-07 19:45:49 +0200

    Selenium.WebDriver.ChromeDriver version changed

--- eBanking/eBanking.IntegrationTests/AutomatedUITests.cs
+++ eBanking/eBanking.IntegrationTests/AutomatedUITests.cs
@@ -14,8 +14,6 @@ namespace eBanking.IntegrationTests
             options.AddArgument("--no-sandbox");
             options.AddArgument("--disable-dev-shm-usage");
             options.AddArgument("--headless");
-            options.AddAdditionalCapability("version", 91.0);
-            //options.BrowserVersion("91.0.4472.77");
             _driver = new ChromeDriver(options);
         }
 
